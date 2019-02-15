8943f0eee40db179e9322a9b2e0fd875ac8e4c6b
Bmbus <www.bmbus@gmail.com>
2019-02-15 17:29:26 +0100

    Add todo

--- utils/cogs.py
+++ utils/cogs.py
@@ -2,7 +2,10 @@ extensions = [
     "cogs.edu.duden",
     "cogs.edu.wiki",
     "cogs.misc.say",
-    #"cogs.coding.MyCode",
+    "cogs.coding.MyCode",
     "cogs.general.tags",
-    "cogs.misc.readme"
+    "cogs.misc.readme",
+    "cogs.general.Idstuff",
+    #"cogs.general.help"
+    "cogs.owner.todo"
 ]
