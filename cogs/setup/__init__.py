cd28302b667f34de3c80a624a463131e8f2a226c
Bmbus <www.bmbus@gmail.com>
2019-03-01 20:36:03 +0100

    Finished setup.py

--- cogs/setup/__init__.py
+++ cogs/setup/__init__.py
@@ -6,3 +6,16 @@ from utils import checks
 
 ON = "<:on_icon:551015396434771968>"
 OFF = "<:off_icon:551015362045673473>"
+
+SETUP_OPTIONS = [
+    "user_join_leave"
+    "tags"
+    "modlog",
+    "meme"
+    "count"
+    "mod",
+    "games",
+    "nsfw",
+    "edu",
+    "social"
+]
--- cogs/setup/setup.py
+++ cogs/setup/setup.py
@@ -3,20 +3,21 @@ from utils.colors import random_color
 import discord
 from disputils import BotEmbedPaginator
 from utils.config import Config
-from . import checks
+from . import checks, SETUP_OPTIONS
+import pprint
 
 class Setup:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(name="setup", aliases=["settings", "options"])
+    @commands.group(name="setup", aliases=["settings", "options"], invoke_without_command=True)
     @commands.has_permissions(manage_guild=True)
     async def _setup(self, ctx):
         """Configure the bot"""
         query = """SELECT * FROM setup WHERE server_id = $1;"""
         row = await self.bot.db.fetchrow(query, ctx.guild.id)
         CHECK = checks.Checks(self.bot, ctx.guild.id, row)
-        embed = [discord.Embed(title=f"All available setup functions - More info's: `{Config().prefix()}setup <FUNCTION/NUMBER>`", color=random_color(),
+        embed = [discord.Embed(title=f"All available setup functions - More info's: `{Config().prefix()}setup <enable/disable> <FUNCTION>`", color=random_color(),
         description="**1. `user_join_leave` : `Enable/disable the bot message, when a user joins/leave`**\n"
                     "**2. `tags` : `Enable/disable server tags`**\n"
                     "**3. `modlog` : `Enable/disable the modolg channel`**\n"
@@ -42,6 +43,47 @@ class Setup:
                  ]
         paginator = BotEmbedPaginator(ctx, embed)
         await paginator.run()
+
+    @_setup.command(name="enable")
+    @commands.has_permissions(manage_guild=True)
+    async def _setup_enable(self, ctx, *, arg:str):
+        """ENABLE A SETUP FUNCTION"""
+
+        if arg in SETUP_OPTIONS:
+            await self._update_db(arg, True, ctx.guild)
+            embed = discord.Embed(title="Yeaahhh", color=random_color(), description=f"You enabled `{arg}`")
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="Oops..", color=random_color(), description="This isn't a setup function...\n\n"f"`{', '.join(SETUP_OPTIONS)}`")
+            await ctx.send(embed=embed)
+
+    @_setup.command(name="disable")
+    @commands.has_permissions(manage_guild=True)
+    async def _setup_disable(self, ctx, *, arg:str):
+        if arg in SETUP_OPTIONS:
+            await self._update_db(arg, False, ctx.guild)
+            embed = discord.Embed(title="Okayy", color=random_color(), description=f"You disabled {arg}")
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(title="Oops..", color=random_color(),description="This isn't a setup function...\n\n"f"`{', '.join(SETUP_OPTIONS)}`")
+            await ctx.send(embed=embed)
+
+
+    async def _update_db(self, arg:str, set:bool, guild):
+        ALIASES = {
+            "user_join_leave": """UPDATE setup SET userjoin_leave = $1 WHERE server_id = $2;""",
+            "tags": """UPDATE setup SET servertags = $1 WHERE server_id = $2;""",
+            "modlog": """UPDATE setup SET modlog = $1 WHERE server_id = $2;""",
+            "meme": """UPDATE setup SET meme = $1 WHERE server_id = $2;""",
+            "count": """UPDATE setup SET counter = $1 WHERE server_id = $2;""",
+            "mod": """UPDATE setup SET mod = $1 WHERE server_id = $2;""",
+            "games": """UPDATE setup SET games = $1 WHERE server_id = $2;""",
+            "nsfw": """UPDATE setup SET nsfw = $1 WHERE server_id = $2;""",
+            "edu": """UPDATE setup SET edu = $1 WHERE server_id = $2;""",
+            "social": """UPDATE setup SET social = $1 WHERE server_id = $2;"""
+        }
+        await self.bot.db.execute(ALIASES[arg], set, guild.id)
+
 
 
 def setup(bot):
